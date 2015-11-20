cf7aedda8c7e49d3c1d3aebe7da58fd1a7180398
Ciaran Synnott <hello@synnott.co.uk>
2015-11-20 08:26:57 +0000

    Update example.php

--- example.php
+++ example.php
@@ -5,3 +5,5 @@ require_once('CarWeb.php');
 use CarWeb\API as CarWebCaller;
 
 $c = new CarWebCaller($username, $password, $client_ref, $client_description, $key);
+
+var_dump($c->search("UK REG", "json"));
